159dcaefc625fab492fb9f529e6b64f2a108fdf8
guoxiaoxin <guoxx@me.com>
2018-01-25 18:55:23 +0800

    Add dummy polygonal light source.

---
+++ Sugar/Graphics/PolygonalAreaLight.cpp
@@ -0,0 +1,260 @@
+/***************************************************************************
+# Copyright (c) 2015, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************/
+#include "Framework.h"
+#include "PolygonalAreaLight.h"
+#include "Utils/Gui.h"
+#include "API/ConstantBuffer.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "Graphics/Model/Model.h"
+#include "glm/gtc/epsilon.hpp"
+
+
+namespace
+{
+    Falcor::Material::SharedPtr createEmissiveMat(glm::vec3 emissiveColor)
+    {
+        Falcor::BasicMaterial basicMat;
+        basicMat.emissiveColor = emissiveColor;
+        return basicMat.convertToMaterial();
+    }
+
+    std::string getEmissiveModelName(const std::string& lightName)
+    {
+        return lightName + "_Emissive";
+    }
+}
+
+namespace Falcor
+{
+
+    PolygonalAreaLight::SharedPtr PolygonalAreaLight::create()
+    {
+        PolygonalAreaLight* pLight = new PolygonalAreaLight{};
+        return SharedPtr(pLight);
+    }
+
+    PolygonalAreaLight::PolygonalAreaLight()
+    {
+        mData.type = LightPolygonal;
+        mData.worldPos = glm::vec3(0.0f);
+        mData.intensity = glm::vec3(1.0f);
+        mpEmissiveMat = createEmissiveMat(mData.intensity);
+
+        // a quad
+        const float r = float(M_SQRT2) / 2.0f;
+        mpVertices.push_back(PolarCoordinate{r, M_PI_4});
+        mpVertices.push_back(PolarCoordinate{r, M_PI_4 + M_PI_2});
+        mpVertices.push_back(PolarCoordinate{r, M_PI_4 + M_PI_2 * 2});
+        mpVertices.push_back(PolarCoordinate{r, M_PI_4 + M_PI_2 * 3});
+
+        createGeometry();
+        updateSurfaceArea();
+    }
+
+    PolygonalAreaLight::~PolygonalAreaLight()
+    {
+        resetGeometry();
+    }
+
+    float PolygonalAreaLight::getPower()
+    {
+        assert(false);
+        return luminance(mData.intensity) * (float)M_PI * mSurfaceArea;
+    }
+
+    void PolygonalAreaLight::renderUI(Gui* pGui, const char* group)
+    {
+        if(!group || pGui->beginGroup(group))
+        {
+            if (mpModelInstance)
+            {
+                {
+                    vec3 t = mpModelInstance->getTranslation();
+                    if (pGui->addFloat3Var("Translation", t, -FLT_MAX, FLT_MAX))
+                    {
+                        mpModelInstance->setTranslation(t, true);
+                    }
+                }
+
+                {
+                    vec3 r = mpModelInstance->getRotation();
+                    r = degrees(r);
+                    if (pGui->addFloat3Var("Rotation", r, -360, 360))
+                    {
+                        r = radians(r);
+                        mpModelInstance->setRotation(r);
+                    }
+                }
+
+                {
+                    vec3 s = mpModelInstance->getScaling();
+                    if (pGui->addFloat3Var("Scaling", s, 0, FLT_MAX))
+                    {
+                        mpModelInstance->setScaling(s);
+                    }
+                }
+            }
+
+            Light::renderUI(pGui);
+
+            if (group)
+            {
+                pGui->endGroup();
+            }
+        }
+    }
+
+    void PolygonalAreaLight::setIntoConstantBuffer(ConstantBuffer* pBuffer, const std::string& varName)
+    {
+        // Upload data to GPU
+        prepareGPUData();
+
+        // Call base class method;
+        Light::setIntoConstantBuffer(pBuffer, varName);
+    }
+
+    void PolygonalAreaLight::setIntoConstantBuffer(ConstantBuffer* pBuffer, size_t offset)
+    {
+        // Upload data to GPU
+        prepareGPUData();
+
+        // Call base class method;
+        Light::setIntoConstantBuffer(pBuffer, offset);
+    }
+
+    void PolygonalAreaLight::prepareGPUData()
+    {
+        // Get the surface area of the geometry mesh
+        mData.surfaceArea = mSurfaceArea;
+
+        // Fetch the mesh instance transformation
+        mData.transMat = mpModelInstance->getTransformMatrix();
+    }
+
+    void PolygonalAreaLight::unloadGPUData()
+    {
+        // Unload GPU data by calling evict()
+    }
+
+    void PolygonalAreaLight::move(const glm::vec3& position, const glm::vec3& target, const glm::vec3& up)
+    {
+        mpModelInstance->move(position, target, up);
+    }
+
+    //void PolygonalAreaLight::setRadius(float r)
+    //{
+    //    if (glm::epsilonEqual(r, mRadius, glm::epsilon<float>()))
+    //    {
+    //        return;
+    //    }
+
+    //    mRadius = r;
+
+    //    resetGeometry();
+    //    createGeometry();
+    //    updateSurfaceArea();
+    //}
+
+    void PolygonalAreaLight::setIntensity(const glm::vec3& intensity)
+    {
+        mData.intensity = intensity;
+        if (mpEmissiveMat)
+        {
+            mpEmissiveMat->setLayerAlbedo(0, glm::vec4(intensity, 0.0f));
+        }
+    }
+
+    void PolygonalAreaLight::addToScene(Scene::SharedPtr pScene)
+    {
+        mpScene = pScene;
+        pScene->addModelInstance(mpModelInstance);
+    }
+
+    void PolygonalAreaLight::setColorFromUI(const glm::vec3 & uiColor)
+    {
+        super::setColorFromUI(uiColor);
+        setIntensity(mData.intensity);
+    }
+
+    void PolygonalAreaLight::setIntensityFromUI(float intensity)
+    {
+        super::setIntensityFromUI(intensity);
+        setIntensity(mData.intensity);
+    }
+
+    void PolygonalAreaLight::resetGeometry()
+    {
+        // cleanup
+		Scene::SharedPtr pScene = mpScene.lock();
+        if (pScene != nullptr && mpModelInstance != nullptr)
+        {
+            for (uint32_t modelId = 0; modelId < pScene->getModelCount(); ++modelId)
+            {
+                Model::SharedPtr pModel = pScene->getModel(modelId);
+                if (mpModelInstance->getObject() == pModel)
+                {
+                    pScene->deleteModel(modelId);
+                    break;
+                }
+            }
+        }
+
+        mpModelInstance = nullptr;
+    }
+
+    void PolygonalAreaLight::createGeometry()
+    {
+        Model::SharedPtr pModel = CreateModelPolygonalPlane(mpVertices, glm::mat4{});
+        ((Mesh::SharedPtr&)pModel->getMesh(0))->setMaterial(mpEmissiveMat);
+
+        mpModelInstance = Scene::ModelInstance::create(pModel, glm::vec3(), glm::vec3(), glm::vec3(1), getEmissiveModelName(mName));
+        setWorldPositionInternal(mData.worldPos);
+
+		Scene::SharedPtr pScene = mpScene.lock();
+        if (pScene)
+        {
+			pScene->addModelInstance(mpModelInstance);
+        }
+    }
+
+    void PolygonalAreaLight::updateSurfaceArea()
+    {
+        // TODO
+        mSurfaceArea = 0;
+    }
+
+    const void PolygonalAreaLight::setName(const std::string& name)
+    {
+        mName = name;
+        if (mpModelInstance)
+        {
+            mpModelInstance->setName(getEmissiveModelName(name));
+        }
+    }
+}
---
+++ Sugar/Graphics/PolygonalAreaLight.h
@@ -0,0 +1,128 @@
+/***************************************************************************
+# Copyright (c) 2015, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************/
+#pragma once
+#include "Graphics/Light.h"
+#include "Graphics/Scene/Scene.h"
+#include "Utils/Geometry/GeometryUtility.h"
+
+namespace Falcor
+{
+    class ConstantBuffer;
+    class Gui;
+
+    /**
+        Area light source
+
+        This class is used to simulate area light sources. All emissive
+        materials are treated as area light sources.
+    */
+    class PolygonalAreaLight : public Light, public std::enable_shared_from_this<PolygonalAreaLight>
+    {
+    public:
+        using super = Light;
+        using SharedPtr = std::shared_ptr<PolygonalAreaLight>;
+        using SharedConstPtr = std::shared_ptr<const PolygonalAreaLight>;
+
+        static SharedPtr create();
+
+        PolygonalAreaLight();
+        ~PolygonalAreaLight();
+        
+        /** Get total light power (needed for light picking)
+        */
+        float getPower() override;
+
+        /** Set the light parameters into a program. To use this you need to include/import 'ShaderCommon' inside your shader.
+            \param[in] pBuffer The constant buffer to set the parameters into.
+            \param[in] varName The name of the light variable in the program.
+        */
+        void setIntoConstantBuffer(ConstantBuffer* pBuffer, const std::string& varName) override;
+
+        /** Set the light parameters into a program. To use this you need to include/import 'ShaderCommon' inside your shader.
+            \param[in] pBuffer The constant buffer to set the parameters into.
+            \param[in] offset Byte offset into the constant buffer to set data to.
+        */
+        void setIntoConstantBuffer(ConstantBuffer* pBuffer, size_t offset) override;
+
+        /** Render UI elements for this light.
+            \param[in] pGui The GUI to create the elements with
+            \param[in] group Optional. If specified, creates a UI group to display elements within
+        */
+        void renderUI(Gui* pGui, const char* group = nullptr) override;
+
+        /** Prepare GPU data
+        */
+        void prepareGPUData() override;
+
+        /** Unload GPU data
+        */
+        void unloadGPUData() override;
+
+        /**
+            IMovableObject interface
+        */
+        void move(const glm::vec3& position, const glm::vec3& target, const glm::vec3& up) override;
+
+        /*
+         *  Set radiance for diffuse emitter
+         */
+        void setIntensity(const glm::vec3& intensity);
+
+        /** Get the light intensity.
+        */
+        const glm::vec3& getIntensity() const { return mData.intensity; }
+
+        /*
+         * Add area light to scene 
+         */
+        void addToScene(Scene::SharedPtr pScene);
+
+        const void setName(const std::string& name);
+
+    private:
+        virtual void setColorFromUI(const glm::vec3& uiColor) override;
+        virtual void setIntensityFromUI(float intensity) override;
+
+
+        void setWorldPositionInternal(const glm::vec3& pos);
+        void resetGeometry();
+        void createGeometry();
+        void updateSurfaceArea();
+
+        std::weak_ptr<Scene> mpScene;
+        Material::SharedPtr mpEmissiveMat;
+        Scene::ModelInstance::SharedPtr mpModelInstance;
+        PolarCoordinateCollection mpVertices;
+
+        float mSurfaceArea = 0.0f;
+
+        constexpr static float mRadiusMin = 0.01f;
+        constexpr static float mRadiusMax = 1000.01f;
+        constexpr static float mRadiusStep = 0.01f;
+    };
+}
